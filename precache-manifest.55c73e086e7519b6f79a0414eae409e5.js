98bb9abc652560334cae82cbf863aebd7f1d6144
Mextli Vazquez <mvazquez@MacBook-Pro-de-Mextli.local>
2019-08-27 02:04:05 -0500

    Updates

---
+++ precache-manifest.55c73e086e7519b6f79a0414eae409e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89f004397bd03b1afc0247fbfed05c4",
+    "url": "/my-api/index.html"
+  },
+  {
+    "revision": "0e43c25b68baecdd7192",
+    "url": "/my-api/static/css/main.0f47acef.chunk.css"
+  },
+  {
+    "revision": "e001e0ae49b6e2fe89c0",
+    "url": "/my-api/static/js/2.5d2be3c6.chunk.js"
+  },
+  {
+    "revision": "0e43c25b68baecdd7192",
+    "url": "/my-api/static/js/main.0e1fe1ea.chunk.js"
+  },
+  {
+    "revision": "99a3e8f2f4eb2a173516",
+    "url": "/my-api/static/js/runtime~main.b4a77de6.js"
+  }
+]);
