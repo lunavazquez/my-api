194f35ad7b77332e45cee0d84a4ffa3b0183f43d
Mextli Vazquez <mvazquez@MacBook-Pro-de-Mextli.local>
2019-08-27 01:49:38 -0500

    new changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users/")
+      .then(response => response.json())
+      .then(json => this.setState({ users: json }));
+  }
+  render() {
+    return (
+      <section className="cardsContainer">
+        {this.state.users.map(user => {
+          return (
+            <div className="card" key={user.id}>
+              <p>{user.name}</p>
+              <p>{user.email}</p>
+              <p>{`${user.address.street} ${user.address.suite} ${user.address.city}`}</p>
+              <p>{user.phone}</p>
+              <p>
+                <a href={user.website}>{`${user.name}'s website`}</a>
+              </p>
+              <p>{user.company.name}</p>
+            </div>
+          );
+        })}
+      </section>
+    );
+  }
 }
 
 export default App;
